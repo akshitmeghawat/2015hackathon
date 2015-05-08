8b7cfd975ffcd2cc83da04ebc2a00ba6c1608bf7
Mahesh Haldar <haldar.mahesh@gmail.com>
2015-05-09 00:37:43 +0530

    sql update

--- sqlFile/hackathon.sql
+++ sqlFile/hackathon.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 08, 2015 at 04:52 PM
+-- Generation Time: May 08, 2015 at 09:07 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.5.19
 
@@ -52,7 +52,7 @@ CREATE TABLE IF NOT EXISTS `developer_project_map` (
 `id` int(11) NOT NULL,
   `dev_id` int(11) NOT NULL,
   `project_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `developer_project_map`
@@ -60,7 +60,12 @@ CREATE TABLE IF NOT EXISTS `developer_project_map` (
 
 INSERT INTO `developer_project_map` (`id`, `dev_id`, `project_id`) VALUES
 (1, 1, 1),
-(2, 2, 1);
+(2, 2, 1),
+(3, 3, 6),
+(4, 4, 7),
+(7, 1, 10),
+(8, 1, 11),
+(9, 1, 12);
 
 -- --------------------------------------------------------
 
@@ -117,16 +122,23 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `start_day` date NOT NULL,
   `end_day` date NOT NULL,
   `description` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `projects`
 --
 
 INSERT INTO `projects` (`project_id`, `name`, `start_day`, `end_day`, `description`) VALUES
-(1, 'Pharm M & A', '2015-05-06', '2015-05-16', 'this is sample project'),
-(2, 'LEG', '2015-05-07', '2015-05-11', 'sample'),
-(3, 'test', '2015-01-07', '2015-06-06', 'asdasd');
+(1, 'Pharm M & A', '2015-05-06', '2015-05-01', 'this is sample project'),
+(2, 'LEG', '2015-05-07', '2015-05-01', 'sample'),
+(3, 'test', '2015-01-07', '2015-06-06', 'asdasd'),
+(6, 'masnd', '2015-02-02', '2016-02-02', 'asd'),
+(7, 'masnd', '2015-04-28', '2015-04-30', 'asd'),
+(8, 'asdas', '2015-04-29', '2015-05-21', 'asd'),
+(9, 'masnd', '2015-05-05', '2015-05-21', ''),
+(10, 'masnd', '2015-05-05', '2015-05-21', 'asd'),
+(11, 'masnd', '2015-04-27', '2015-05-14', 'asd'),
+(12, '', '2015-04-28', '2015-05-13', 'asd');
 
 -- --------------------------------------------------------
 
@@ -138,7 +150,7 @@ CREATE TABLE IF NOT EXISTS `project_po_map` (
 `id` int(11) NOT NULL,
   `po_id` int(11) NOT NULL,
   `project_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `project_po_map`
@@ -146,7 +158,11 @@ CREATE TABLE IF NOT EXISTS `project_po_map` (
 
 INSERT INTO `project_po_map` (`id`, `po_id`, `project_id`) VALUES
 (1, 1, 1),
-(2, 2, 2);
+(2, 2, 2),
+(3, 3, 6),
+(4, 5, 7),
+(8, 1, 11),
+(9, 1, 12);
 
 -- --------------------------------------------------------
 
@@ -181,7 +197,7 @@ ALTER TABLE `developers`
 -- Indexes for table `developer_project_map`
 --
 ALTER TABLE `developer_project_map`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `dev_id` (`dev_id`), ADD KEY `project_id_2` (`project_id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `project_id_2` (`project_id`), ADD KEY `dev_id` (`dev_id`);
 
 --
 -- Indexes for table `login`
@@ -205,7 +221,7 @@ ALTER TABLE `projects`
 -- Indexes for table `project_po_map`
 --
 ALTER TABLE `project_po_map`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `po_id` (`po_id`), ADD UNIQUE KEY `project_id` (`project_id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `po_id` (`po_id`), ADD KEY `project_id` (`project_id`);
 
 --
 -- Indexes for table `tech_skills`
@@ -226,7 +242,7 @@ MODIFY `dev_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 -- AUTO_INCREMENT for table `developer_project_map`
 --
 ALTER TABLE `developer_project_map`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `login`
 --
@@ -241,12 +257,12 @@ MODIFY `po_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `project_po_map`
 --
 ALTER TABLE `project_po_map`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `tech_skills`
 --
@@ -260,8 +276,8 @@ MODIFY `tech_skills_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 -- Constraints for table `developer_project_map`
 --
 ALTER TABLE `developer_project_map`
-ADD CONSTRAINT `developer_project_map_ibfk_1` FOREIGN KEY (`dev_id`) REFERENCES `developers` (`dev_id`),
-ADD CONSTRAINT `developer_project_map_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`);
+ADD CONSTRAINT `developer_project_map_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`),
+ADD CONSTRAINT `developer_project_map_ibfk_3` FOREIGN KEY (`dev_id`) REFERENCES `developers` (`dev_id`);
 
 --
 -- Constraints for table `project_po_map`
