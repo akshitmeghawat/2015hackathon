7d3a1c6861b12fd5edf7d943f718ab02b8c45e99
Mahesh Haldar <haldar.mahesh@gmail.com>
2015-05-09 07:43:31 +0530

    adding link

--- developer/project_by_tech.php
+++ developer/project_by_tech.php
@@ -2,7 +2,7 @@
 include '../connection.php';
 $skill = $_GET['skill'];
 echo $skill;
-$query = "SELECT name,description FROM projects WHERE tech LIKE '%$skill%';";
+$query = "SELECT project_id, name,description FROM projects WHERE tech LIKE '%$skill%';";
 $result = mysql_query($query);
 $res='<tr class="first-row">
                           <td colspan="1"></td>
@@ -10,7 +10,7 @@ $res='<tr class="first-row">
                           <td colspan="5">Description</td>
                       </tr>';
 while ($row = mysql_fetch_array($result)) {
-  $res=$res."<tr><td colspan='1'></td><td colspan='2'>".$row['name']."</td><td colspan='5'>".$row['description']."</td></tr>";
+  $res=$res."<tr><td colspan='1'></td><td colspan='2'><a href='../admin/project_details.php?id=".$row['project_id']."'>".$row['name']."</a></td><td colspan='5'>".$row['description']."</td></tr>";
 }
 echo $res;
 ?>
