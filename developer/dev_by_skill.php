4091cb5cfb76716081fe6fc730008a5478be6caf
Akshit Meghawat <am1292@gmail.com>
2015-05-09 09:11:44 +0530

    Removed region

--- developer/dev_by_skill.php
+++ developer/dev_by_skill.php
@@ -2,15 +2,15 @@
 include '../connection.php';
 $skill = $_GET['skill'];
 echo $skill;
-$query = "SELECT name,region FROM developers WHERE tech LIKE '%$skill%';";
+$query = "SELECT name,tech FROM developers WHERE tech LIKE '%$skill%';";
 $result = mysql_query($query);
 $res='<tr class="first-row">
                           <td colspan="1"></td>
                           <td colspan="2">Name</td>
-                          <td colspan="5">Region</td>
+                          <td colspan="5">Tech</td>
                       </tr>';
 while ($row = mysql_fetch_array($result)) {
-  $res.="<tr><td colspan='1'></td><td colspan='2'>".$row['name']."</td><td colspan='5'>".$row['region']."</td></tr>";
+  $res.="<tr><td colspan='1'></td><td colspan='2'>".$row['name']."</td><td colspan='5'>".$row['tech']."</td></tr>";
 }
 echo $res;
 ?>
