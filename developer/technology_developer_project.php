78d866974f6cf10730f17dba9abd0bd0843224b5
Jaditi <Jaditi.1993@gmail.com>
2015-05-09 07:36:36 +0530

    technology for each project

--- developer/technology_developer_project.php
+++ developer/technology_developer_project.php
@@ -1,7 +1,7 @@
 <?php
 include '../connection.php';
-$skill = $_POST['tech-submit'];
-$dev_id = $_POST['id'];
+$skill = $_GET['tech-submit'];
+$dev_id = $_GET['id'];
 echo $skill;
 echo $dev_id;
 $query = "UPDATE developer_project_map SET tech='".$skill."' where dev_id = '".$dev_id."'";
